deaa63d59078f3935a49639e131baea37a087654
user <woentron@gmail.vom>
2025-10-14 23:11:23 +0300

    Personal Movie DB

---
+++ 19/lesson19.js
@@ -0,0 +1,22 @@
+// Практика
+"use strict"
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели ?', '');
+
+const personalMovieDB = {
+    'count': numberOfFilms,
+    'movies': {},
+    'actor': {},
+    'genres': [],
+    'privat': false,
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько цените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько цените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
